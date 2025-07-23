ae90df723c36f00e5e90e50b1ce79e42e145939a
Vinicius Silva <viniciussilvapereira24@gmail.com>
2025-07-23 00:12:17 -0300

    configuração do Zod

--- src/controllers/SignUpController.ts
+++ src/controllers/SignUpController.ts
@@ -1,9 +1,31 @@
 
+import z, { email } from "zod";
+
 import { HttpRequest, HttpResponse } from "../types/Http";
-import { ok } from "../utils/http";
+import { badRequest, created } from "../utils/http";
+
+const schema = z.object({
+    goal: z.enum(["lose", "maintain", "gain"]),
+    gender: z.enum(["male", "female"]),
+    birthDate: z.iso.date(),
+    height: z.number(),
+    weight: z.number(),
+    activityLevel: z.number().min(1).max(5),
+    account: z.object({
+        email: z.email(),
+        name: z.string().min(1),
+        password: z.string().min(8)
+    })
+});
 
 export class SignUpController {
-    static async handle(request: HttpRequest): Promise<HttpResponse> {
-        return ok({accessToken: "funcionou o cadastro"});
+    static async handle({ body }: HttpRequest): Promise<HttpResponse> {
+        const { success, error, data } = schema.safeParse(body);
+
+        if (!success) {
+            return badRequest({ errors: error.issues });
+        }
+
+        return created({ accessToken: "funcionou o cadastro" });
     }
 }
