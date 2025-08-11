22a50fb05a3c9069bfd89783854426b5bd4a1977
Vinicius Silva <viniciussilvapereira24@gmail.com>
2025-08-10 21:48:34 -0300

    corrige chave de arquivo de entrada ao criar refeição

--- src/controllers/CreateMealController.ts
+++ src/controllers/CreateMealController.ts
@@ -34,7 +34,7 @@ export class CreateMealController {
         const [meal] = await db.insert(mealsTable)
             .values({
                 userId: userId,
-                inputFileKey: "input_file_key",
+                inputFileKey: fileKey,
                 inputType: data?.fileType === "audio/m4a" ? "audio" : "picture",
                 status: "uploading",
                 icon: "",
